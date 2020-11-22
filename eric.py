ffe86b6c8ae574a0f69917498ce7a5306e543cd7
Eric Smrkovsky <33237852+Ericsmrk@users.noreply.github.com>
2020-11-21 23:17:46 -0800

    trying to add sample from mdp_apps.ipynb

---
+++ eric.py
@@ -0,0 +1,54 @@
+#Trying an example
+from mdp import *
+
+#sample transition matrix
+t = {
+    'leisure': {
+                    'facebook': {'leisure':0.9, 'class1':0.1},
+                    'quit': {'leisure':0.1, 'class1':0.9},
+                    'study': {},
+                    'sleep': {},
+                    'pub': {}
+               },
+    'class1': {
+                    'study': {'class2':0.6, 'leisure':0.4},
+                    'facebook': {'class2':0.4, 'leisure':0.6},
+                    'quit': {},
+                    'sleep': {},
+                    'pub': {}
+              },
+    'class2': {
+                    'study': {'class3':0.5, 'end':0.5},
+                    'sleep': {'end':0.5, 'class3':0.5},
+                    'facebook': {},
+                    'quit': {},
+                    'pub': {},
+              },
+    'class3': {
+                    'study': {'end':0.6, 'class1':0.08, 'class2':0.16, 'class3':0.16},
+                    'pub': {'end':0.4, 'class1':0.12, 'class2':0.24, 'class3':0.24},
+                    'facebook': {},
+                    'quit': {},
+                    'sleep': {}
+              },
+    'end': {}
+}
+
+#sample rewards
+rewards = {
+    'class1': 4,
+    'class2': 6,
+    'class3': 10,
+    'leisure': -1,
+    'end': 0
+}
+
+terminals = ['end']
+
+init = 'class1'
+
+our_mdp = CustomMDP(init, terminals, t, rewards, gamma=.9)
+#our_mdp = CustomMDP(t, rewards, terminals, init, gamma=.9)
+print("mem:", our_mdp)
+
+value_iteration(our_mdp)
